7b2ee3a125e5f4f1f90e3c4d6dd76a865fe5e289
razzaq1140 <162542388+razzaq1140@users.noreply.github.com>
2025-02-17 15:05:12 +0500

    front-end code complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,11 @@ import 'package:flutter/material.dart';
 
 
 import 'package:investorentrepreneur/screen/home/filter/provider/filter_provider.dart';
-<<<<<<< HEAD
 import 'package:investorentrepreneur/screen/home/homescreen.dart';
 import 'package:investorentrepreneur/screen/jobs/screen/apply_to_tangent_screen.dart';
 import 'package:investorentrepreneur/screen/jobs/screen/job_filter_screen.dart';
 import 'package:investorentrepreneur/screen/jobs/screen/jobs_screen.dart';
 import 'package:investorentrepreneur/screen/podcast/podcastcategory.dart';
-=======
-
->>>>>>> d165df3c399c134cff0ad6fe1d5f65780142f409
-
 import 'package:investorentrepreneur/splashscreen/splashscreen.dart';
 import 'package:provider/provider.dart';
 
